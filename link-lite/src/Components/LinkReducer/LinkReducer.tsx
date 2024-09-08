f913956292bef3985cb1d7bbdf60c869c4cd8ae0
Antoine Stouff <antoine-78280@hotmail.fr>
2024-09-08 17:23:13 +0200

    feat/add files and architecture

---
+++ link-lite/src/Components/LinkReducer/LinkReducer.tsx
@@ -0,0 +1,9 @@
+function LinkReducer() {
+  return (
+    <div>
+      <h1>LinkReducer</h1>
+    </div>
+  );
+};
+
+export default LinkReducer;
---
+++ link-lite/src/Components/UserCredentials/UserCredentials.tsx
@@ -0,0 +1,9 @@
+function UserCredentials() {
+  return (
+    <div>
+      <h1>UserCredentials</h1>
+    </div>
+  );
+};
+
+export default UserCredentials;
