0fd61e158ef206b784fc78bce41908213ea3e2b1
Antoine Stouff <antoine-78280@hotmail.fr>
2024-09-23 09:08:54 +0200

    feat/add signup component

--- link-lite/src/Components/UserCredentials/UserSignUp.tsx
+++ link-lite/src/Components/UserCredentials/UserSignUp.tsx
@@ -1,38 +1,82 @@
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import authApiHandler from '../../services/authApiHandler';
+
+type createUserFormState = {
+  first_name: string,
+  email: string,
+  password: string
+}
+
 function UserSignUp() {
+  const [formData, setFormData] = useState<createUserFormState>({
+    first_name: "",
+    email: "",
+    password: ""
+  });
+  const [error, setError] = useState("")
+  const navigate = useNavigate()
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      await authApiHandler.signup(formData);
+      navigate("/login/");
+    } catch(err) {
+      setError('Error during user creation.');
+      console.error(err);
+    };
+  }
+
   return (
     <div>
       <h1>UserSignUp</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            name="first_name"
+            value={formData.first_name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        <button type="submit">Sign Up!</button>
+      </form>
     </div>
   );
 };
 
 export default UserSignUp;
 
-// import React from 'react';
-// import { useHistory } from 'react-router-dom';
-// import axios from 'axios';
-
-// const Signup: React.FC = () => {
-//   const history = useHistory();
-  
-//   const handleSignup = async (event: React.FormEvent) => {
-//     event.preventDefault();
-//     const formData = new FormData(event.target as HTMLFormElement);
-
-//     try {
-//       await axios.post('/api/register/', formData);
-//       history.push('/check-your-email');  // Redirige vers la page de confirmation d'e-mail
-//     } catch (error) {
-//       console.error('Erreur lors de l\'inscription', error);
-//     }
-//   };
-
-//   return (
-//     <form onSubmit={handleSignup}>
-//       {/* Champs du formulaire d'inscription */}
-//       <button type="submit">S'inscrire</button>
-//     </form>
-//   );
-// };
-
-// export default Signup;
