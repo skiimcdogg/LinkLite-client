0976936c57eb968bc12f23f29e1d114780b14971
Antoine Stouff <antoine-78280@hotmail.fr>
2024-09-23 09:18:14 +0200

    feat/add signin component and remove axios useless import

--- link-lite/src/Components/UserCredentials/UserSignUp.tsx
+++ link-lite/src/Components/UserCredentials/UserSignUp.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
 import authApiHandler from '../../services/authApiHandler';
 
 type createUserFormState = {
