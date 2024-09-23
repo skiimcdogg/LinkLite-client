b431bac97e930bb365def7d927899fd85bb69135
Antoine Stouff <antoine-78280@hotmail.fr>
2024-09-23 16:24:18 +0200

    feat/add link reducer component and modify user check mail

--- link-lite/src/Components/UserCredentials/UserCheckEmail.tsx
+++ link-lite/src/Components/UserCredentials/UserCheckEmail.tsx
@@ -1,7 +1,7 @@
 function UserCheckEmail() {
   return (
     <div>
-      <h1>UserCheckEmail</h1> 
+      <h1>Check your Emails !</h1> 
       {/* Juste une phrase pour dire qu'il faut check le mail */}
     </div>
   );
